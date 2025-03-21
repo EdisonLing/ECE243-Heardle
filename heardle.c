3489a94bc7d7e0d582cc58d14b9120aca39e756b
MrEdisonthebest <109105268+EdisonLing@users.noreply.github.com>
2025-03-21 15:57:35 -0400

    pollKeyboard checks if value is one of the valid values (used in program) and prints past 3 PS2 inputs based off of that

--- heardle.c
+++ heardle.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +7,15 @@
 #define PS2_BASE 0xFF200100  // PS2 base address
 #define RVALID_BIT 0x8000    // RVALID bit of PS2_Data register
 #define PS2_DATA_BITS 0xFF   // data bits of PS2_Data register
+
+// constants for PS2 code to actual key
+#define KEY_1 0x16
+#define KEY_2 0x1e
+#define KEY_3 0x26
+#define KEY_4 0x25
+#define KEY_Q 0x15
+#define KEY_SPACE 0x29
+#define KEY_NULL 0xF0
 
 #define HEX_3to0_BASE 0xFF200020
 #define HEX_5to4_BASE 0xFF200030
@@ -16,22 +26,32 @@ typedef struct {
 
 keyboard_keys_struct keyboard_keys;
 
+char validKeysArr[] = {KEY_1, KEY_2, KEY_3, KEY_4, KEY_Q, KEY_SPACE, KEY_NULL};
+int sizeOfValidKeysArr = sizeof(validKeysArr) / sizeof(validKeysArr[0]);
+
+bool isValidKey(char key) {
+    for (int i = 0; i < sizeOfValidKeysArr; i++) {
+        if (key == validKeysArr[i]) return true;
+    }
+    return false;
+}
+
 void pollKeyboard() {
     volatile int *PS2_ptr = (int *)PS2_BASE;
-    int PS2_data = *(PS2_ptr);           // get value of PS2 data register
+    int PS2_data = *(PS2_ptr);  // get value of PS2 data register
+    // printf("%d\n", PS2_data);
     int RVALID = PS2_data & RVALID_BIT;  // get only the RVALID bit
     if (RVALID != 0) {
         // shift values queue back and add newest keypress to front
         keyboard_keys.last_last_key = keyboard_keys.last_key;
         keyboard_keys.last_key = keyboard_keys.key;
         keyboard_keys.key = PS2_data & PS2_DATA_BITS;
-        char keyPrintStatement[100] = "Keys: ";
-        strcat(keyPrintStatement, keyboard_keys.key);
-        strcat(keyPrintStatement, ", ");
-        strcat(keyPrintStatement, keyboard_keys.last_key);
-        strcat(keyPrintStatement, ", ");
-        strcat(keyPrintStatement, keyboard_keys.last_last_key);
-        strcat(keyPrintStatement, '\n');
+        // printf("Keys: %x, %x, %x\n", keyboard_keys.key, keyboard_keys.last_key, keyboard_keys.last_last_key);
+        char key = 0x00, last_key = 0x00, last_last_key = 0x00;  // let FF be none
+        if (isValidKey(keyboard_keys.key)) key = keyboard_keys.key;
+        if (isValidKey(keyboard_keys.last_key)) last_key = keyboard_keys.last_key;
+        if (isValidKey(keyboard_keys.last_last_key)) last_last_key = keyboard_keys.last_last_key;
+        printf("Valid Keys: %x, %x, %x\n", key, last_key, last_last_key);
     }
 }
 
