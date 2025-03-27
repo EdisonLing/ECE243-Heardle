597e9f7df158ac8d1e3230f00c41c36c4ad66286
MrEdisonthebest <109105268+EdisonLing@users.noreply.github.com>
2025-03-26 23:21:32 -0400

    made substitude title screen work, and go next stage on spacebar, also created writeCharacter, writeWord, and more

--- heardle.c
+++ heardle.c
@@ -3,8 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define LEDR_BASE 0xFF200000
+
+#define PIXEL_BUFFER_BASE 0xFF203020
+
+volatile int pixel_buffer_start;  // global variable
+short int Buffer1[240][512];      // 240 rows, 512 (320 + padding) columns
+short int Buffer2[240][512];
+
+#define CHARACTER_BUFFER_BASE 0x09000000  // base for drawing characters
 
 #define PS2_BASE 0xFF200100  // PS2 base address
 #define RVALID_BIT 0x8000    // RVALID bit of PS2_Data register
@@ -26,14 +33,163 @@
 #define HEX_3to0_BASE 0xFF200020
 #define HEX_5to4_BASE 0xFF200030
 
+// valid keys for pollKeyboard
+char validKeysArr[] = {
+    KEY_1,
+    KEY_2,
+    KEY_3,
+    KEY_4,
+    KEY_Q,
+    KEY_SPACE,
+    KEY_NULL,
+    KEY_ENTER,
+    KEY_RIGHT_ARROW,
+    KEY_W};
+int sizeOfValidKeysArr = sizeof(validKeysArr) / sizeof(validKeysArr[0]);  // dynamically calculates size from array
+
+typedef struct {
+    int x, y, prevx, prevy, dx, dy;
+    short int colour;
+} Box;
+
+/*
+GLOBAL VARIABLES
+GLOBAL VARIABLES
+GLOBAL VARIABLES
+*/
+int PlayerScore = 0;      // total score
+int RoundDifficulty = 1;  // the number of seconds the music will play for, inversely proportional to amount of points gained on correct answer
+/*
+GLOBAL VARIABLES
+GLOBAL VARIABLES
+GLOBAL VARIABLES
+*/
+
+/*
+FUNCTION PROTOTYPES
+FUNCTION PROTOTYPES
+FUNCTION PROTOTYPES
+*/
+void clearScreen();
+bool isValidKey(char key);
+bool checkAnswer(int answer_index, int round_index);
+bool submitAnswer(int answer_index, int round_index);
+void pollKeyboard();
+void drawCharacter();
+void clearCharacterBuffer();
+void writeCharacter(char character, int x, int y);
+void drawStartScreen();
+
+/*
+FUNCTION PROTOTYPES
+FUNCTION PROTOTYPES
+FUNCTION PROTOTYPES
+*/
+
 typedef struct {
     char key, last_key, last_last_key;
 } keyboard_keys_struct;
 
 keyboard_keys_struct keyboard_keys = {KEY_NULL, KEY_NULL, KEY_NULL};
 
-char validKeysArr[] = {KEY_1, KEY_2, KEY_3, KEY_4, KEY_Q, KEY_SPACE, KEY_NULL, KEY_ENTER, KEY_RIGHT_ARROW, KEY_W};
-int sizeOfValidKeysArr = sizeof(validKeysArr) / sizeof(validKeysArr[0]);
+int main(void) {
+    volatile int *pixel_ctrl_ptr = (int *)PIXEL_BUFFER_BASE;
+    /* set front pixel buffer to Buffer 1 */
+    *(pixel_ctrl_ptr + 1) = (int)&Buffer1;  // first store the address in the  back buffer
+    /* now, swap the front/back buffers, to set the front buffer location */
+    wait_for_vsync();
+    /* initialize a pointer to the pixel buffer, used by drawing functions */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clearScreen();  // pixel_buffer_start points to the pixel buffer
+
+    /* set back pixel buffer to Buffer 2 */
+    *(pixel_ctrl_ptr + 1) = (int)&Buffer2;
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // we draw on the back buffer
+    clearScreen();                               // pixel_buffer_start points to the pixel buffer
+    clearCharacterBuffer();
+
+    /*
+    DRAW THE START SCREEN
+    */
+    drawStartScreen();
+    /*
+    WAIT FOR USER INPUT TO PROCEED
+    */
+    while (!(keyboard_keys.key == KEY_SPACE && keyboard_keys.last_last_key == KEY_NULL)) {
+        pollKeyboard();
+    }
+    clearScreen();
+    clearCharacterBuffer();
+    while (1) {
+        pollKeyboard();
+
+        wait_for_vsync();
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // new back buffer
+    }
+    return 0;
+}
+
+void writeCharacter(char character, int x, int y) {
+    // pointer to character buffer
+    volatile char *char_buffer = (char *)CHARACTER_BUFFER_BASE;
+
+    // format word
+    int offset = (y << 7) + x;
+
+    // write word into memory
+    char_buffer[offset] = character;
+    return;
+}
+
+int getStringSize(char *word) {
+    int size = 0;
+    while (word[size] != '\0') {
+        size++;
+    }
+    printf("'%s' is %d characters long\n", word, size);
+    return size;
+}
+
+void writeWord(char *word, int x0, int y0) {
+    int size = getStringSize(word);
+    printf("writing word at %d, %d\n", x0, y0);
+    for (int i = 0; i < size; i++) {
+        writeCharacter(word[i], i + x0, y0);
+    }
+}
+
+void clearCharacterBuffer() {
+    for (int x = 0; x < 80; x++) {
+        for (int y = 0; y < 60; y++) {
+            writeCharacter(' ', x, y);
+        }
+    }
+}
+
+void plotPixel(int x, int y, short int line_color) {
+    // Make sure we're within bounds
+    if (x < 0 || x >= 320 || y < 0 || y >= 240)
+        return;
+
+    volatile short int *one_pixel_address;
+    one_pixel_address = (volatile short int *)(pixel_buffer_start + (y << 10) + (x << 1));
+    *one_pixel_address = line_color;
+}
+
+void clearScreen() {  // loops through each pixel and sets to black
+    printf("started clearing screen\n");
+    for (int x = 0; x < 320; x++) {
+        for (int y = 0; y < 240; y++) {
+            plotPixel(x, y, 0x0);  // 0xFFFFFF
+        }
+    }
+}
+
+// WIP
+void drawStartScreen() {  // clears screen then draws title text
+    char str[] = "HEARDLE";
+    writeWord(&str, 5, 5);
+}
 
 bool isValidKey(char key) {
     for (int i = 0; i < sizeOfValidKeysArr; i++) {
@@ -41,9 +197,6 @@ bool isValidKey(char key) {
     }
     return false;
 }
-
-int PlayerScore = 0;      // total score
-int RoundDifficulty = 1;  // the number of seconds the music will play for, inversely proportional to amount of points gained on correct answer
 
 bool checkAnswer(int answer_index, int round_index) {  // temp, will return whether or not user's answer is correct
     return true;
@@ -91,11 +244,49 @@ void pollKeyboard() {
     }
 }
 
-int main(void) {
-    volatile int *LEDR = LEDR_BASE;
-    while (1) {
-        pollKeyboard();
-        LEDR = PlayerScore;
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void drawLine(int x0, int y0, int x1, int y1, short int colour) {
+    bool is_steep = abs(y1 - y0) > abs(x1 - x0);
+    if (is_steep) {
+        swap(&x0, &y0);
+        swap(&x1, &y1);
     }
-    return 0;
+    if (x0 > x1) {
+        swap(&x0, &x1);
+        swap(&y0, &y1);
+    }
+    int deltax = x1 - x0;
+    int deltay = abs(y1 - y0);
+
+    int error = -(deltax / 2);
+    int y = y0;
+
+    int y_step;
+    if (y0 < y1) y_step = 1;
+    else y_step = -1;
+
+    for (int x = x0; x < x1; x++) {
+        if (is_steep) plotPixel(y, x, colour);
+        else plotPixel(x, y, colour);
+        error += deltay;
+        if (error > 0) {
+            y += y_step;
+            error -= deltax;
+        }
+    }
+}
+
+void wait_for_vsync() {
+    volatile int *fbuf = (int *)0xFF203020;  // base of VGA controller
+    int status;
+    *fbuf = 1;             // enable swap
+    status = *(fbuf + 3);  // read status register
+    while (status & 1) {   // wait until we poll status = 0
+        status = *(fbuf + 3);
+    }
 }
